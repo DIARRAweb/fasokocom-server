9afe33e4d54e8fb854d40ecbd5e83ce6480abb24
Idrissa DIARRA <diarraidrissa400@gmail.com>
2026-04-04 02:05:09 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("✅ Un utilisateur est connecté :", socket.id);
+
+  socket.on("join", (data) => {
+    console.log("👤 Nom reçu :", data.name);
+
+    // prévenir les autres
+    socket.broadcast.emit("user-joined");
+  });
+
+  socket.on("offer", (offer) => {
+    socket.broadcast.emit("offer", offer);
+  });
+
+  socket.on("answer", (answer) => {
+    socket.broadcast.emit("answer", answer);
+  });
+
+  socket.on("ice-candidate", (candidate) => {
+    socket.broadcast.emit("ice-candidate", candidate);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ Un utilisateur s'est déconnecté :", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
